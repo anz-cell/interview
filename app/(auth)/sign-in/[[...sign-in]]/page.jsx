4995853b373663bf38e0975010d499dedd23c9bd
anz-cell <mathematicsgeek1@outlook.com>
2024-08-04 01:34:09 +0400

    "thrid coomit"

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -31,10 +31,7 @@ export default function Page() {
               Welcome to AI Interview Mocker 🦑
             </h2>
     
-            <p className="mt-4 leading-relaxed text-white/90">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-              quibusdam aperiam voluptatum.
-            </p>
+            
           </div>
         </section>
     
@@ -62,13 +59,10 @@ export default function Page() {
               </a>
     
               <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                Welcome to Squid 🦑
+                Welcome to AI Interview 🦑
               </h1>
     
-              <p className="mt-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-                quibusdam aperiam voluptatum.
-              </p>
+              
             </div>
     
             <SignIn />
